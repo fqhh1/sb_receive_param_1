d317d18eb5ca051ee4454430acc0dd613f727057
fq <fangqi12306@gmail.com>
2023-12-10 10:12:13 +0800

    提交测试

---
+++ src/test/java/com/fq/SbReceiveParamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbReceiveParamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
